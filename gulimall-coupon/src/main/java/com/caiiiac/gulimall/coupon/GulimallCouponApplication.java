9b5bc7fb330143ba2894ef9cdc9ed4793d847b0e
caijq <caiiiac@163.com>
2021-04-30 16:17:33 +0800

    change springboot version

--- gulimall-coupon/src/main/java/com/caiiiac/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/caiiiac/gulimall/coupon/GulimallCouponApplication.java
@@ -3,7 +3,9 @@ package com.caiiiac.gulimall.coupon;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @MapperScan("com.caiiiac.gulimall.coupon.dao")
 @SpringBootApplication
 public class GulimallCouponApplication {
@@ -11,5 +13,6 @@ public class GulimallCouponApplication {
     public static void main(String[] args) {
         SpringApplication.run(GulimallCouponApplication.class, args);
     }
+
 
 }
--- gulimall-member/src/main/java/com/caiiiac/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/caiiiac/gulimall/member/GulimallMemberApplication.java
@@ -3,7 +3,9 @@ package com.caiiiac.gulimall.member;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @MapperScan("com.caiiiac.gulimall.member.dao")
 @SpringBootApplication
 public class GulimallMemberApplication {
