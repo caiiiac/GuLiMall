c9f2686303fbdef6514e3cfe222f644c5e324713
caijq <caiiiac@163.com>
2021-05-06 15:13:06 +0800

    add spring-cloud

--- gulimall-coupon/src/main/java/com/caiiiac/gulimall/coupon/controller/CouponController.java
+++ gulimall-coupon/src/main/java/com/caiiiac/gulimall/coupon/controller/CouponController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,14 +26,26 @@ import com.caiiiac.common.utils.R;
  * @email caiiiac@163.com
  * @date 2021-04-29 15:03:35
  */
+
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
 
+    @Value("${coupon.user.name}")
+    private String name;
+    @Value("${coupon.user.age}")
+    private Integer age;
+
+    @RequestMapping("/test/user")
+    public R testUser() {
+        return R.ok().put("name", name)
+                .put("age", age);
+    }
     /**
-     * 测试 nacos
+     * 测试 nacos服务注册
      * @return
      */
     @RequestMapping("/member/list")
