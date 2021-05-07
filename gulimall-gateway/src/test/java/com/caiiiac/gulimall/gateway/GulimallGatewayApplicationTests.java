f3d42f11cf1fc997387e823c0e5990cc8af87f1a
caijq <caiiiac@163.com>
2021-05-07 16:13:55 +0800

    add gateway

---
+++ gulimall-gateway/src/test/java/com/caiiiac/gulimall/gateway/GulimallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiiiac.gulimall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
