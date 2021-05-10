b86062226f32343b972bb796c4f7e7fa91039519
caijq <caiiiac@163.com>
2021-05-10 15:06:54 +0800

    add vo

--- gulimall-product/src/main/java/com/caiiiac/gulimall/product/service/impl/AttrServiceImpl.java
+++ gulimall-product/src/main/java/com/caiiiac/gulimall/product/service/impl/AttrServiceImpl.java
@@ -1,7 +1,21 @@
 package com.caiiiac.gulimall.product.service.impl;
 
+import com.caiiiac.gulimall.product.dao.AttrAttrgroupRelationDao;
+import com.caiiiac.gulimall.product.dao.AttrGroupDao;
+import com.caiiiac.gulimall.product.dao.CategoryDao;
+import com.caiiiac.gulimall.product.entity.AttrAttrgroupRelationEntity;
+import com.caiiiac.gulimall.product.entity.AttrGroupEntity;
+import com.caiiiac.gulimall.product.entity.CategoryEntity;
+import com.caiiiac.gulimall.product.vo.AttrRespVo;
+import com.caiiiac.gulimall.product.vo.AttrVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -11,10 +25,22 @@ import com.caiiiac.common.utils.Query;
 import com.caiiiac.gulimall.product.dao.AttrDao;
 import com.caiiiac.gulimall.product.entity.AttrEntity;
 import com.caiiiac.gulimall.product.service.AttrService;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+import org.w3c.dom.Attr;
 
 
 @Service("attrService")
 public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements AttrService {
+
+    @Autowired
+    AttrAttrgroupRelationDao relationDao;
+
+    @Autowired
+    AttrGroupDao attrGroupDao;
+
+    @Autowired
+    CategoryDao categoryDao;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -24,6 +50,66 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
         );
 
         return new PageUtils(page);
+    }
+
+    @Transactional
+    @Override
+    public void saveAttr(AttrVo attr) {
+        AttrEntity entity = new AttrEntity();
+
+        BeanUtils.copyProperties(attr, entity);
+        // 保存基本数据
+        this.save(entity);
+
+        // 保存关联关系
+        AttrAttrgroupRelationEntity relationEntity = new AttrAttrgroupRelationEntity();
+        relationEntity.setAttrGroupId(attr.getAttrGroupId());
+        relationEntity.setAttrId(attr.getAttrId());
+        relationDao.insert(relationEntity);
+    }
+
+    @Override
+    public PageUtils queryBaseAttrPage(Map<String, Object> params, Long catelogId) {
+
+        QueryWrapper<AttrEntity> wrapper = new QueryWrapper<>();
+
+        if (catelogId != 0) {
+            wrapper.eq("catelog_id", catelogId);
+        }
+        String key = (String) params.get("key");
+        if (!StringUtils.isEmpty(key)) {
+            wrapper.and((w) -> {
+                w.eq("attr_id", key).or().like("attr_name", key);
+            });
+        }
+
+        IPage<AttrEntity> page = this.page(
+                new Query<AttrEntity>().getPage(params),
+                wrapper
+        );
+        PageUtils pageUtils = new PageUtils(page);
+        List<AttrEntity> records = page.getRecords();
+        List<AttrRespVo> respVos = records.stream().map((attrEntity) -> {
+            AttrRespVo attrRespVo = new AttrRespVo();
+            BeanUtils.copyProperties(attrEntity, attrRespVo);
+
+            AttrAttrgroupRelationEntity attrAttrgroupRelationEntity = relationDao.selectOne(
+                    new QueryWrapper<AttrAttrgroupRelationEntity>().eq("attr_id", attrEntity.getAttrId())
+            );
+            if (attrAttrgroupRelationEntity != null) {
+                AttrGroupEntity attrGroupEntity = attrGroupDao.selectById(attrAttrgroupRelationEntity.getAttrGroupId());
+                attrRespVo.setGroupName(attrGroupEntity.getAttrGroupName());
+            }
+
+            CategoryEntity categoryEntity = categoryDao.selectById(attrEntity.getCatelogId());
+            if (categoryEntity != null) {
+                attrRespVo.setCatelogName(categoryEntity.getName());
+            }
+            return attrRespVo;
+        }).collect(Collectors.toList());
+
+        pageUtils.setList(respVos);
+        return pageUtils;
     }
 
 }
---
+++ gulimall-product/src/main/java/com/caiiiac/gulimall/product/vo/AttrRespVo.java
@@ -0,0 +1,16 @@
+package com.caiiiac.gulimall.product.vo;
+
+import lombok.Data;
+
+@Data
+public class AttrRespVo extends AttrVo {
+
+    /**
+     * 所属分类名称
+     */
+    private String catelogName;
+    /**
+     * 所属分组名称
+     */
+    private String groupName;
+}
